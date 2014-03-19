52420f2156a2e5ad7737b829601cd9bc5739591c
Matthew O'Connell <matthew.j.oconnell1@gmail.com>
2014-03-19 15:16:32 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+InfoBytes::Application.config.secret_key_base = '8966a8bbc19a85d8a0d734f3d1e985cebc39019c8163372ba94225c52d9f7293d9c4002a74393f4d09bee1adc4918c84e5e344698e52ab380afdd6d507150263'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+InfoBytes::Application.config.session_store :cookie_store, key: '_InfoBytes_session'

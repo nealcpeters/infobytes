10e370aa71c4a1020ce79cf59ef61837103fddfa
Matthew O'Connell <matthew.j.oconnell1@gmail.com>
2014-03-19 15:39:27 -0500

    add devise for user signup and auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 InfoBytes::Application.routes.draw do
+
+  root to: "home#index"
+
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

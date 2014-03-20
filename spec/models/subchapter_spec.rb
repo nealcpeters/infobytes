f87ed06c9d9bb96db0c753b4db23e57206925630
Amara Getzell <agetzell@gmail.com>
2014-03-20 15:56:54 -0500

    added validations and corresponding tests for subtopics

--- spec/models/subchapter_spec.rb
+++ spec/models/subchapter_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
-describe Subchapter do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# describe Subchapter do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
--- spec/models/subtopic_spec.rb
+++ spec/models/subtopic_spec.rb
@@ -7,5 +7,15 @@ describe Subtopic do
       	Topic.create(title: "Movies").subtopics.create(title: "Star Wars", description: "Tutorials Relating to the Epic Heptology")
     	}.to change(Subtopic, :count).by(1)
     end
+    it "it does not create a subtopic without a title" do
+      expect{
+      	Topic.create(title: "Movies").subtopics.create(description: "Tutorials Relating to the Epic Heptology")
+    	}.to change(Subtopic, :count).by(0)
+   end
+   it "it does not create a subtopic without a description" do
+      expect{
+      	Topic.create(title: "Movies").subtopics.create(title: "Star Wars")
+    	}.to change(Subtopic, :count).by(0)
+    end
    end
 end

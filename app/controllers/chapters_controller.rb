dcff14fbc72665c91cc23ad6a8d36d8c4d63e508
Matthew O'Connell <matthew.j.oconnell1@gmail.com>
2014-03-20 11:00:16 -0500

    add show and new to chapter

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -1,2 +1,38 @@
 class ChaptersController < ApplicationController
+
+	def new
+    @tutorial = Tutorial.find(params[:tutorial_id])
+    @chapter = Chapter.new
+	end 
+
+	def create
+    @tutorial = Tutorial.find(params[:tutorial_id])
+    @chapter = @tutorial.chapters.new(chapter_params)
+
+    if @chapter.save
+      flash[:notice] = "Chapter #{@chapter.number}: #{@chapter.title} added."
+      redirect_to @chapter
+    else
+      @errors = @chapter.errors.messages
+      render 'new'
+    end
+	end
+
+	def edit
+
+	end
+
+	def update
+
+	end
+
+	def destroy
+
+	end
+
+	private
+
+	def chapter_params
+    params.require(:chapter).permit(:title, :number)
+	end
 end

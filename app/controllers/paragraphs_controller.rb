aa246bbb28447cd836986abe6dc9fb5e3214e85d
Alexandru Codreanu <alexcodreanu86@yahoo.com>
2014-03-20 16:54:41 -0500

    added links to create paragraph, image, code_snippets, and added new and create to paragraphs controller

--- app/controllers/paragraphs_controller.rb
+++ app/controllers/paragraphs_controller.rb
@@ -1,4 +1,31 @@
 class ParagraphsController < ApplicationController
   def new
+    @sub_chapter = SubChapter.find(params[:sub_chapter_id])
+    @paragraph = Paragraph.new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def create
+    @sub_chapter = SubChapter.find(params[:sub_chapter_id])
+    @paragraph = Paragraph.new(paragraph_params)
+    if @paragraph.save
+      @paragraph.content = Content.create(sub_chapter_id: params[:sub_chapter_id], order_number: (@sub_chapter.contents.count + 1))
+      flash[:notice]="Your new paragraph has been added!"
+      redirect_to @sub_chapter
+    else
+      @errors = @paragraph.errors.messages
+      render "new"
+    end
+  end
+
+  protected
+
+  def paragraph_params
+    params.require(:paragraph).permit(:body)
   end
 end

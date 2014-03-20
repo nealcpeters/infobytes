f957bd02e146665daf351823e0d3aa351f22a79c
Alexandru Codreanu <alexcodreanu86@yahoo.com>
2014-03-20 10:57:24 -0500

    added views for new tutorial and form and link to create chapter

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140320022833) do
+ActiveRecord::Schema.define(version: 20140320150446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "chapters", force: true do |t|
+    t.string   "title"
+    t.integer  "number"
+    t.integer  "tutorial_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "subtopics", force: true do |t|
     t.string   "title"

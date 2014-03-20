a279aed3ffac65ce0d40df4e3493281841722148
Matthew O'Connell <matthew.j.oconnell1@gmail.com>
2014-03-20 18:00:19 -0500

    add image contorller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140320212014) do
+ActiveRecord::Schema.define(version: 20140320224701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,15 @@ ActiveRecord::Schema.define(version: 20140320212014) do
     t.integer  "attatchable_id"
     t.string   "attatchable_type"
     t.integer  "sub_chapter_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "images", force: true do |t|
+    t.string   "image_path_file_name"
+    t.string   "image_path_content_type"
+    t.integer  "image_path_file_size"
+    t.datetime "image_path_updated_at"
     t.datetime "created_at"
     t.datetime "updated_at"
   end

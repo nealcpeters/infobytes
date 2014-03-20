ec699a6102b1c2e2a325701d4ef451ad71acd912
Neal Peters <nealcpeters@gmail.com>
2014-03-20 07:43:52 -0500

    committing so i can check out master

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,12 @@
 require "spec_helper"
 
 describe User do
-  context "when displaying the user's entire name" do
+  context "when displaying the user's first name" do
     it "is a combination of first and last name" do
+      user = User.new(first_name: "Han", last_name: "Solo", user_name: "rebel_scum", email: "han@aliance.com", encrypted_password: "password")
+      expect(user.first_name).to eq("Han")
 
+      puts "test"
     end
   end
 end

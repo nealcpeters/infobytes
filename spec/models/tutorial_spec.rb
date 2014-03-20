03cf51327fef3f94f3cf1f82b173d83ca4bc3d53
Neal Peters <nealcpeters@gmail.com>
2014-03-20 13:36:41 -0500

    all user model testing done.  tutorial testing in progress

--- spec/models/tutorial_spec.rb
+++ spec/models/tutorial_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe Tutorial do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # context "when creating a tutorial" do
+  #   it "it creates a valid tutorial" do
+  #     expect{
+  #     	User.create(first_name: "Han", last_name: "Solo", user_name: "rebel_scum", email: "han@alliance.com", password: "password", password_confirmation: "password")
+  #     	Tutorial.create(title: "Destroy the Death Star", description: "How to find small exhaust ports", rating: 1, user: 1, subtopic: "Programming")
+  #   	}.to change(Tutorial, :count).by(1)
+  #   end
+
+  #   it "it will not create a tutorial without a title" do
+  #   	expect{
+  #     	User.create(first_name: "Han", last_name: "Solo", user_name: "rebel_scum", email: "han@alliance.com", password: "password", password_confirmation: "password")
+  #     	Tutorial.create(description: "How to find small exhaust ports", rating: 1, user: 1, subtopic: "Programming")
+  #   	}.to change(User, :count).by(0)
+  # 	end
+
+  # 	it "it will not create a tutorial without a description" do
+  # 		expect{
+  # 			User.create(first_name: "Han", last_name: "Solo", user_name: "rebel_scum", email: "han@alliance.com", password: "password", password_confirmation: "password")
+  #     	Tutorial.create(title: "Destroy the Death Star", rating: 1, user: 1, subtopic: "Programming")
+  # 		}.to change(User, :count).by(0)
+  # 	end  	
+  # end
 end

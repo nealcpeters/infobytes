2377755352c4ff97161831cfb5824ed3c9eff0a0
Amara Getzell <agetzell@gmail.com>
2014-03-20 18:36:59 -0500

    added validation and test for sub_chapters

--- spec/models/sub_chapter_spec.rb
+++ spec/models/sub_chapter_spec.rb
@@ -8,11 +8,11 @@ context "when creating a sub-chapter" do
 	        }.to change(SubChapter, :count).by(1)
 	    end
 
-	 xit "it will not create a sub-chapter without a title" do
+	it "it will not create a sub-chapter without a title" do
 	    expect{
-	      	User.create(first_name: "Han", last_name: "Solo", user_name: "rebel_scum", email: "han@alliance.com", password: "password", password_confirmation: "password").tutorials.create(title: "Destroy the Death Star", description: "How to find small exhaust ports").chapters.create(title: nil).sub_chapters.create(title: nil)
-	    	}.to change(Chapter, :count).by(0)
+	      	User.create(first_name: "Han", last_name: "Solo", user_name: "rebel_scum", email: "han@alliance.com", password: "password", password_confirmation: "password").tutorials.create(title: "Destroy the Death Star", description: "How to find small exhaust ports").chapters.create(title: "what is star wars?").sub_chapters.create(title: nil)
+	        }.to change(SubChapter, :count).by(0)
 	  	end
  	
-	 end
+	end
 end

7133b64f818967ad0f6b6505c99cae90862244e2
Matthew O'Connell <matthew.j.oconnell1@gmail.com>
2014-03-20 19:11:27 -0500

    add s3 credintials to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,13 @@ InfoBytes::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.paperclip_defaults = {
+    :storage => :s3
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end

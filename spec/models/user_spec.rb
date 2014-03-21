bbd00bbd15c9b4e58fcf8ab08b8ff802a7520f7c
Amara Getzell <agetzell@gmail.com>
2014-03-21 13:59:25 -0500

    added additional user_name validations and their corresponding tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,5 +26,24 @@ describe User do
   			User.create(first_name: "Luke", last_name: "Skywalker", user_name: "i_kissed_my_sister", email: "han@alliance.com", password: "password", password_confirmation: "password")
   		}.to change(User, :count).by(1)
   	end
+
+    it "will not create a user with a non-unique user name" do
+      expect{
+        User.create(first_name: "Han", last_name: "Solo", user_name: "rebel_scum", email: "han@alliance.com", password: "password", password_confirmation: "password")
+        User.create(first_name: "Luke", last_name: "Skywalker", user_name: "rebel_scum", email: "luke@alliance.com", password: "password", password_confirmation: "password")
+      }.to change(User, :count).by(1)
+    end
+
+    it "will not create a user with a user name longer than 32 characters" do
+      expect{
+        User.create(first_name: "Han", last_name: "Solo", user_name: "rebel_scumxxxxxxxxxxxxxxxxxxxxxxx", email: "han@alliance.com", password: "password", password_confirmation: "password")
+      }.to change(User, :count).by(0)
+    end
+
+    it "will not create a user name with characters other than alphanumeric and underscores" do
+      expect{
+        User.create(first_name: "Han", last_name: "Solo", user_name: "*rebel_scum*", email: "han@alliance.com", password: "password", password_confirmation: "password")
+      }.to change(User, :count).by(0)
+    end
   end
 end

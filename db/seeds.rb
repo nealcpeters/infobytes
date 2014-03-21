c90dda9dfc77396c95b19326a4b64bd95aa9ddfb
Neal Peters <nealcpeters@gmail.com>
2014-03-21 14:44:58 -0500

    working on seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
 # HEY DO SUB_CHAPTERS NEED AN ORDER NUMBER LIKE CHAPTERS?
 # FYI CODE SNIPPETS ARE JUST THE INTERNAL BITS, NO NEED TO SAVE ALL THE DIVS I WAS WRONG
 
-user = User.create(first_name: "Han", last_name: "Solo", user_name: "rebel_scum", email: "han@aliance.com", password: "password", password_confirmation: "password")
+user = User.create(first_name: "Abed", last_name: "Nadir", user_name: "abed", email: "abed@greendale.com", password: "password", password_confirmation: "password")
+User.create(first_name: "Troy", last_name: "Barnes", user_name: "troy", email: "troy@greendale.com", password: "password", password_confirmation: "password")
+User.create(first_name: "Jeff", last_name: "Winger", user_name: "jeff", email: "jeff@greendale.com", password: "password", password_confirmation: "password")
+User.create(first_name: "Annie", last_name: "Edison", user_name: "annie", email: "annie@greendale.com", password: "password", password_confirmation: "password")
+User.create(first_name: "Britta", last_name: "Perry", user_name: "britta", email: "britta@greendale.com", password: "password", password_confirmation: "password")
 
 Topic.create(title: "Programming")
 Topic.create(title: "Networking")
@@ -9,15 +13,29 @@ Topic.create(title: "Security")
 Topic.create(title: "Unix")
 Topic.create(title: "Computer Hardware")
 
+#Programming subtopics
 Subtopic.create(title: "Ruby", description: "A dynamic, open source programming language with a focus on simplicity and productivity.", topic_id: 1)
 Subtopic.create(title: "Python", description: "Python is a widely used general-purpose, high-level programming language.", topic_id: 1)
+Subtopic.create(title: "Java", description: "Java is a class-based, object-oriented programming language developed by Sun Microsystems in the 1990s.", topic_id: 1)
+Subtopic.create(title: "C", description: "A general-purpose, imperative programming language developed in the early '70s, C is the oldest and most widely used language, providing the building blocks for other popular languages, such as C#, Java, JavaScript and Python.", topic_id: 1)
+Subtopic.create(title: "C++", description: "C++ is an intermediate-level language with object-oriented programming features, originally designed to enhance the C language.", topic_id: 1)
+Subtopic.create(title: "C#", description: "Pronounced 'C-sharp', C# is a multi-paradigm language developed by Microsoft as part of its .NET initiative. ", topic_id: 1)
+Subtopic.create(title: "Objective-C", description: "Objective-C is a general-purpose, object-oriented programming language used by the Apple operating system.", topic_id: 1)
+Subtopic.create(title: "PHP", description: "PHP (Hypertext Processor) is a free, server-side scripting language designed for dynamic websites and app development.", topic_id: 1)
+Subtopic.create(title: "Javascript", description: "JavaScript is a client and server-side scripting language developed by Netscape that derives much of its syntax from C.", topic_id: 1)
+Subtopic.create(title: "SQL", description: "Structured Query Language (SQL) is a special-purpose language for managing data in relational database management systems.", topic_id: 1)
+
+
+
+
+
 Subtopic.create(title: "Data Network", description: "A computer network or data network is a telecommunications network that allows computers to exchange data.", topic_id: 2)
 Subtopic.create(title: "Network Nodes", description: "Network computer devices that originate, route and terminate the data are called network nodes.", topic_id: 2)
 
 Tutorial.create(title: "How to Be Amara", description: "A detailed tutorial on how to be the most gnarly land lubber of a poodle", user_id: 1, rating: 3, subtopic_id: 1)
 Tutorial.create(title: "How to Be Eric", description: "A detailed tutorial on how to be the most gnarly land lubber of a historian", user_id: 1, rating: 3, subtopic_id: 1)
-Tutorial.create(title: "How to Be Matt", description: "A detailed tutorial on how to be the most gnarly land lubber of a freight train", user_id: 1, rating: 3, subtopic_id: 2)
-tutorial = Tutorial.create(title: "How to Be Neal", description: "A detailed tutorial on how to be the most gnarly land lubber of a sailor", user_id: 1, rating: 3, subtopic_id: 2)
+Tutorial.create(title: "How to Be Matt", description: "A detailed tutorial on how to be the most gnarly land lubber of a freight train", user_id: 1, rating: 3, subtopic_id: 1)
+tutorial = Tutorial.create(title: "How to Be Neal", description: "A detailed tutorial on how to be the most gnarly land lubber of a sailor", user_id: 1, rating: 3, subtopic_id: 1)
 intro = Chapter.create(title: "Introduction", number: 0, tutorial_id: tutorial.id)
  
 ch_one = Chapter.create(title: "Not Giving a Fuck", number: 1, tutorial_id: tutorial.id)

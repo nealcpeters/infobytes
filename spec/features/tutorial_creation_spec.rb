f09e81c07386b012ac40cc1a0e12a3dff4c6014a
Eric Stensland <estensland88@gmail.com>
2014-03-21 17:26:02 -0500

    fixed code snippet requirement before creation

--- spec/features/tutorial_creation_spec.rb
+++ spec/features/tutorial_creation_spec.rb
@@ -11,6 +11,7 @@ feature 'Tutorial Creation' do
     fill_in 'user[email]', with: "tutorial_tester@aliance.com"
     fill_in 'user[password]', with: "password"
     click_button "Sign in"
+    
   end  
 
   context "for a non-logged in user accessing the route" do
@@ -22,6 +23,13 @@ feature 'Tutorial Creation' do
   end
 
   context "for a logged user" do
+    scenario "the form must direct to must direct to page with created fields present" do
+      visit "/tutorials/new"
+      fill_in 'tutorial[description]', with: "ghijk"
+      click_button "Create Tutorial"
+      expect(page).to have_content("can't be blank")
+    end
+    
     before :each do
       visit "/tutorials/new"
       fill_in 'tutorial[title]', with: "abcdef"
@@ -36,6 +44,11 @@ feature 'Tutorial Creation' do
 
     scenario "must direct to page with success alert message" do
       expect(page).to have_content("Your new tutorial has been created!")
+    end
+
+    scenario "clicking on the add chapter bring add chapter form up" do
+      click_link "Add Chapter"
+      expect(page).to have_content("Title")      
     end
   end
 end

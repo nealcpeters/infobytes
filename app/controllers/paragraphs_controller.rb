ef69190d6c8ae44be6df8b7bc5968a8797de3109
Matthew O'Connell <matthew.j.oconnell1@gmail.com>
2014-03-22 10:24:24 -0500

    add jquery to append paragraphs

--- app/controllers/paragraphs_controller.rb
+++ app/controllers/paragraphs_controller.rb
@@ -16,6 +16,7 @@ class ParagraphsController < ApplicationController
     @paragraph = Paragraph.find(params[:id])
     @paragraph.update(paragraph_params)
     @sub_chapter = @paragraph.content.sub_chapter
+    
     if @paragraph.save
       flash[:notice]="Your paragraph has been updated!"
       redirect_to @sub_chapter
@@ -28,7 +29,7 @@ class ParagraphsController < ApplicationController
   def create
     @sub_chapter = SubChapter.find(params[:sub_chapter_id])
     @paragraph = Paragraph.new(paragraph_params)
-    
+
     if @paragraph.save
       @paragraph.content = Content.create(sub_chapter_id: params[:sub_chapter_id], order_number: (@sub_chapter.contents.count + 1))
       

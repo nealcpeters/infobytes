127799341a97dd7a9a2e962a5e0ccc5840cb704e
Eric Stensland <estensland88@gmail.com>
2014-03-22 10:15:41 -0500

    specs for image uploader not uploading non image

--- spec/features/image_creation_spec.rb
+++ spec/features/image_creation_spec.rb
@@ -44,5 +44,12 @@ feature 'Image Creation' do
       expect(page).should have_content 'Image path'
     end
 
+    scenario "with a non-image file redirects to image#new view with an error message" do
+      attach_file("image[image_path]", File.expand_path('db/seeds.rb'))
+      click_button "Create Image"
+      expect(page).should have_content 'is invalid'
+      expect(page).should have_content 'Image path'
+    end
+
   end
 end

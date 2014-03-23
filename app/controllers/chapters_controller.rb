82a290a2c7aefe6a0aca335de20e42a51280d819
Alexandru Codreanu <alexcodreanu86@yahoo.com>
2014-03-23 18:15:11 -0500

    fixed the ordering in chapters

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -11,7 +11,7 @@ class ChaptersController < ApplicationController
 	def create
     @tutorial = Tutorial.find(params[:tutorial_id])
 
-    @chapter = @tutorial.chapters.new(chapter_params(@tutorial))
+    @chapter = @tutorial.chapters.new(chapter_params.merge(number: @tutorial.chapters.count + 1))
     
     if @chapter.save
      
@@ -35,7 +35,7 @@ class ChaptersController < ApplicationController
 
 	def update
 		@chapter = Chapter.find(params[:id])
-    @chapter.update_attributes(chapter_params(@chapter.tutorial))
+    @chapter.update_attributes(chapter_params)
 
     if @chapter.save
       flash[:notice] = "Chapter Updated"
@@ -70,8 +70,8 @@ class ChaptersController < ApplicationController
 
 	private
 
-	def chapter_params(tutorial)
-    params.require(:chapter).permit(:title).merge(number: tutorial.chapters.count + 1)
+	def chapter_params
+    params.require(:chapter).permit(:title)
 	end
 
 	def authenticate_tutorial_owner_create

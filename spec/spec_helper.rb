ad0f2c38c2d100c05a40b77c93a3ade78497dfa2
Eric Stensland <estensland88@gmail.com>
2014-03-24 15:32:52 -0500

    altered gemfile and spec as Ryan recommended to hopefully fix the coveralls problem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,4 @@
-require 'coveralls'
-require "factory_girl_rails"
 
-Coveralls.wear!
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
@@ -10,6 +7,11 @@ require 'rspec/rails'
 require 'rspec/autorun'
 require "capybara/rails"
 require 'database_cleaner'
+
+require 'coveralls'
+require "factory_girl_rails"
+
+Coveralls.wear!('rails')
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.

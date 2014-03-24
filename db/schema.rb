4776993ac3341489ba1cdae9d05c3c5d336d0c08
Amara Getzell <agetzell@gmail.com>
2014-03-24 17:30:13 -0500

    schema fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20140324211323) do
-
-
+ActiveRecord::Schema.define(version: 20140324215418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -118,6 +115,7 @@ ActiveRecord::Schema.define(version: 20140324211323) do
   create_table "tutorials", force: true do |t|
     t.string   "title"
     t.text     "description"
+    t.integer  "rating"
     t.integer  "user_id"
     t.integer  "subtopic_id"
     t.datetime "created_at"

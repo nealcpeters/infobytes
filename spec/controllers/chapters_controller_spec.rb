da8044f5afc97fcd70ed674fde1ac987d0966923
Eric Stensland <estensland88@gmail.com>
2014-03-25 14:08:54 -0500

    finished chapter controller specs, remove depracated methods

--- spec/controllers/chapters_controller_spec.rb
+++ spec/controllers/chapters_controller_spec.rb
@@ -26,16 +26,15 @@ describe ChaptersController do
 
   describe "Create route" do
     
-
-    it "must redirects if save'" do
+    it "must redirects if save" do
       post :create, tutorial_id: @tutorial.id, chapter: {title: "tuttut", number: 42} 
       expect(response).to render_template(nil)
     end
 
-    xit "must create a tutorial if saved" do
+    it "must create a chapter if saved" do
       expect{
-        post :create, tutorial: {title: "tuttut", number: 42, user_id: @user.id, tutorial_id: @tutorial.id}
-      }.to change(Tutorial, :count).by(1) 
+      post :create, tutorial_id: @tutorial.id, chapter: {title: "tuttut", number: 42} 
+      }.to change(Chapter, :count).by(1) 
     end
   end
 
@@ -47,52 +46,32 @@ describe ChaptersController do
   end
 
   describe "Update route" do
-    xit "must render edit view if it does not save'" do
-      patch :update, id: @chapter.id, tutorial: {id: @chapter.id, description: nil}
-      expect(response).to render_template("edit")
-    end
-
-    xit "must redirect to @tutorial upon save'" do
-      patch :update, id: @tutorial.id, tutorial: {id: @tutorial.id, description: "blah"}
-      expect(response).to redirect_to(@tutorial) 
+    it "must redirect to @tutorial upon save'" do
+      patch :update, id: @chapter.id, chapter: {number: 1}
+      expect(response).to redirect_to(@chapter) 
     end
   end
 
   describe "Destroy route" do
-    xit "must route to tutorials#destroy'" do
-      {delete: "/tutorials/#{@tutorial.id}"}.should route_to(
+    it "must route to chapter#destroy'" do
+      {delete: "/chapters/#{@chapter.id}"}.should route_to(
         action: 'destroy',
-        controller: "tutorials",
-        id: (@tutorial.id.to_s)
+        controller: "chapters",
+        id: (@chapter.id.to_s)
       )
     end
 
-    xit "must destroy a record given proper params'" do
+    it "must destroy a record given proper params'" do
       expect{
-        delete :destroy, id: @tutorial.id
-      }.to change(Tutorial, :count).by(-1)     
-    end
-  end
-  
-  describe "Index route" do
-    xit "must render index view'" do
-      get :index, page: 1
-      expect(response).to render_template("index")
+        delete :destroy, id: @chapter.id
+      }.to change(Chapter, :count).by(-1)     
     end
   end
 
-  describe "HTML_view route" do
-    xit "must render html_view view'" do
-      chapter_create
-      get 'html_view', id: @tutorial.id
-      expect(response).to render_template("html_view")
-    end
-  end
-
-  describe "search route" do
-    xit "must render search view'" do
-      get :search, :params => {search_data: @tutorial.title}
-      expect(response).to render_template("search")
+  describe "Generate route" do
+    it "must render html_view view'" do
+      post 'generate', tutorial_id: @tutorial.id
+      expect(response).to redirect_to(@tutorial)
     end
   end
 

af19012153af75dee2f2bd7e702d2ac4003e54a5
Neal Peters <nealcpeters@gmail.com>
2014-03-25 08:42:39 -0500

    show and sign in tests for user

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,39 @@
-# require 'spec_helper'
+describe UsersController do
+  include Devise::TestHelpers
 
-# describe UsersController do
+  before :each do
+    @user = create(:user)
+    sign_in @user
+  end
 
-# end
+  describe "Show route" do
+    it "must render show view" do
+      get 'show', :id => @user.id
+      expect(response).to render_template("show")
+    end
+  end
+
+  describe "Sign in" do
+    it "must render new view" do
+      get 'sign_in'
+      expect(response).to render_template("devise/sessions/_form")
+    end
+  end
+
+  # describe "Create route" do
+  #   it "must render new view if without a save'" do
+  #     post :create, tutorial: {id: @tutorial.id, description: @tutorial.description, user_id: @user.id}
+  #     expect(response).to render_template("new")
+  #   end
+
+  #   it "must redirects if save'" do
+  #     post :create, tutorial: {title: "tuttut", description: "basd", user_id: @user.id}
+  #     expect(response).to render_template(nil)
+  #   end
+
+  #   it "must create a tutorial if saved" do
+  #     expect{
+  #       post 'create', tutorial: {id: @tutorial.id, title: "tuttttorial", description: "woohoo"}
+  #     }.to change(Tutorial, :count).by(1) 
+  #   end
+end  

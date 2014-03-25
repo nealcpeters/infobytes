5da5b4391532d0fd763df28a8e23c29be6a409db
Neal Peters <nealcpeters@gmail.com>
2014-03-25 15:34:31 -0500

    fixed button mispelling on search results

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -7,17 +7,17 @@ class Tutorial < ActiveRecord::Base
 
   validates :title, :description, :user_id, presence: true
 
-  def self.search(search)
-  	search_condition = "%" + search + "%"
-  	find(:all, :conditions => ['title LIKE ? OR description LIKE ?', search_condition, search_condition])
-	end
+ #  def self.search(search)
+ #  	search_condition = "%" + search + "%"
+ #  	find(:all, :conditions => ['title LIKE ? OR description LIKE ?', search_condition, search_condition])
+	# end
 
-  def update_chapter_order
-    self.chapters.order(:number).each_with_index do |chap, index|
-      chap.number = index + 1
-      chap.save
-    end
-  end
+ #  def update_chapter_order
+ #    self.chapters.order(:number).each_with_index do |chap, index|
+ #      chap.number = index + 1
+ #      chap.save
+ #    end
+ #  end
 
   
 end

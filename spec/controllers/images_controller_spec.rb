762ba25c43ff883f1b6e4f798787ed457db42279
Neal Peters <nealcpeters@gmail.com>
2014-03-25 10:06:31 -0500

    images controller update test

--- spec/controllers/images_controller_spec.rb
+++ spec/controllers/images_controller_spec.rb
@@ -20,8 +20,29 @@ describe ImagesController do
 
   describe "edit route" do
     it "must render edit route" do
-      get 'edit', :id => @image.id
+      post 'edit', :id => @image.id
       expect(response).to render_template("edit")
     end
+  end
+
+  describe "update route" do
+    it "must redirect to sub chapter if saved" do
+      get 'update', id: @image.id, image: {image_path_file_name: "test9.jpg", image_path_content_type: "image/jpeg", image_path_file_size: 6853}
+      expect(response).to render_template(@subchapter)
+    end
+
+    it "must render new view if save not successful" do
+      get 'update', id: @image.id, image: {image_path_file_name: nil, image_path_content_type: "image/jpeg", image_path_file_size: 6853}
+      expect(response).to render_template("new")
+    end
+
+  end
+
+  describe "create route" do
+    it "must redirect to subchapter if new image successful" do
+      post 'create', sub_chapter_id: @sub_chapter.id, image: {image_path_file_name: "test2.jpg", image_path_content_type: "image/jpeg", image_path_file_size: 6851} 
+      expect(response).to render_template("new")
+    end
+
   end
 end

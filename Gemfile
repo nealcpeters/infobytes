596aa7c909a0e8eabc6a418784a8b2870669284f
Matthew O'Connell <matthew.j.oconnell1@gmail.com>
2014-03-26 12:00:13 -0500

    add sidekiq

--- Gemfile
+++ Gemfile
@@ -43,6 +43,8 @@ gem 'aws-sdk'
 
 gem "paperclip", "~> 4.1"
 
+gem 'sidekiq'
+
 
 group :development, :test do
   gem 'pry'
--- app/models/image.rb
+++ app/models/image.rb
@@ -5,6 +5,7 @@ class Image < ActiveRecord::Base
 																   original: '600x800>'
 																 }, 
 																 :default_url => "/images/:style/missing.png"
+  process_in_background :image_path
 
   validates_attachment_content_type :image_path, :content_type => /\Aimage\/(jpg|jpeg|pjpeg|png|x-png)\Z/, :message => 'This file type is not allowed (only jpg/png images).'  
   validates :image_path, :attachment_presence => true

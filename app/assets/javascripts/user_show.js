5b182d980f78c6749ea98826d74d9d207471ab24
Amara Getzell <agetzell@gmail.com>
2014-03-26 16:19:46 -0500

    removed mailer username/password from dev file

---
+++ app/assets/javascripts/user_show.js
@@ -0,0 +1,12 @@
+$(function(){
+
+	$(document).on('click', ".delete-community-link", function(e){
+    e.preventDefault();
+      console.log("hello world")
+      
+      // var url = $(this).attr("action");
+      // var data = $(this).serialize()
+      // createCommunity(url, data);
+
+    })
+})

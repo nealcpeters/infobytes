387b2eba19345bc585f2b429c1e0bbda97931407
Matthew O'Connell <matthew.j.oconnell1@gmail.com>
2014-03-27 10:50:06 -0500

    add gravatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,17 @@
 class User < ActiveRecord::Base
+  include Gravtastic
+  gravtastic(:secure => "true",
+             :rating => "G",
+             :size => 250) 
+  
   has_many :tutorials
   has_many :chapters, through: :tutorials
   has_and_belongs_to_many :communities
   has_many :comments
   has_many :ratings
+
+  has_gravatar
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -15,6 +23,10 @@ class User < ActiveRecord::Base
   	:length => { :maximum => 32 }
 
  	validates_format_of :user_name, :with => /\A[a-zA-Z0-9_]*[a-zA-Z][a-zA-Z0-9_]*\z/ 
+
+  def fullname
+    "#{self.first_name} #{self.last_name}" 
+  end
 end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140324215418) do
+ActiveRecord::Schema.define(version: 20140327134109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -142,6 +142,7 @@ ActiveRecord::Schema.define(version: 20140324215418) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "description"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
